4c857186b03672bc866753e48250ba3d60866c5c
D.J. Crosby <taladan@gmail.com>
2023-03-06 18:43:02 -0500

    Initial commit

---
+++ substrings.rb
@@ -0,0 +1,22 @@
+# substrings.rb
+#
+
+dictionary = %w[
+  below
+  down
+  go
+  going
+  horn
+  how
+  howdy
+  it
+  i
+  low
+  own
+  part
+  partner
+  sit
+]
+
+def substrings(string, dict = dictionary)
+end
