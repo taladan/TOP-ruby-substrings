98018abf0d1260531ceb02213bc0b831e4812687
D.J. Crosby <taladan@gmail.com>
2023-03-07 08:14:06 -0500

    Parse string words and dict words
    Return counts of dictionary words that appear in the string passed
    Makes judicious use of #tap method to pass the new hash in for
    assignment.

--- substrings.rb
+++ substrings.rb
@@ -1,5 +1,6 @@
 # substrings.rb
 #
+require "pry-byebug"
 
 dictionary = %w[
   below
@@ -18,5 +19,19 @@ dictionary = %w[
   sit
 ]
 
-def substrings(string, dict = dictionary)
+def subs(string, dict)
+  string.downcase!
+  str_array = string.split().to_a
+
+  wf =
+    Hash
+      .new(0)
+      .tap do |h|
+        str_array.each do |word|
+          dict.each { |dword| h[dword] += 1 if word.include?(dword) }
+        end
+      end
 end
+
+wf2 = subs("Howdy partner, sit down! How's it going?", dictionary)
+puts wf2
